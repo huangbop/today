897e9c547fb09e64a0855c7a7ff0fc3649901b47
Huang Bo <huangbop@gmail.com>
2015-07-20 02:27:29 +0800

    With uwsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -15,6 +15,8 @@ framework.
 """
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
