52c3ff61e9311209ab1c2b88291e30e95aabf321
Huang Bo <huangbop@gmail.com>
2015-08-19 10:41:27 +0800

    Api views only with products

--- api/urls.py
+++ api/urls.py
@@ -5,10 +5,7 @@ from products.views import ProductViewSet
 
 
 router = routers.DefaultRouter()
-router.register(r'snippets', SnippetViewSet)
 router.register(r'products', ProductViewSet)
-router.register(r'users', UserViewSet)
-router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
