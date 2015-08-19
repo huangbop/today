51be02dcdcabb160da10b6c50f9b4d6c037847a6
Huang Bo <huangbop@gmail.com>
2015-08-19 10:47:01 +0800

    Product model fields remove all default value

--- products/models.py
+++ products/models.py
@@ -4,15 +4,15 @@ from stdimage.utils import UploadToClassNameDir
 
 
 class Product(models.Model):
-    is_show = models.BooleanField(default=True)
+    is_show = models.BooleanField()
     image = StdImageField(upload_to=UploadToClassNameDir(), blank=True)
     title = models.CharField(max_length=100, blank=True)
     description = models.CharField(max_length=500, blank=True)
-    is_new = models.BooleanField(default=False)
-    is_hot = models.BooleanField(default=False)
-    is_pep = models.BooleanField(default=False)
-    rate = models.FloatField(default=5)
-    price = models.FloatField(default=0)
+    is_new = models.BooleanField()
+    is_hot = models.BooleanField()
+    is_pep = models.BooleanField()
+    rate = models.FloatField()
+    price = models.FloatField()
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
