f4d8e7a9fe2a31d9ba86f3e57e7586f56560c7ee
Huang Bo <huangbop@gmail.com>
2015-08-19 10:57:53 +0800

    Product model boolean fields default

--- products/models.py
+++ products/models.py
@@ -4,15 +4,15 @@ from stdimage.utils import UploadToClassNameDir
 
 
 class Product(models.Model):
-    is_show = models.BooleanField()
+    is_show = models.BooleanField(default=True)
     image = StdImageField(upload_to=UploadToClassNameDir(), blank=True)
     title = models.CharField(max_length=100, blank=True)
     description = models.CharField(max_length=500, blank=True)
-    is_new = models.BooleanField()
-    is_hot = models.BooleanField()
-    is_pep = models.BooleanField()
-    rate = models.FloatField()
-    price = models.FloatField()
+    is_new = models.BooleanField(default=False)
+    is_hot = models.BooleanField(default=False)
+    is_pep = models.BooleanField(default=False)
+    rate = models.FloatField(default=5)
+    price = models.FloatField(default=0)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
