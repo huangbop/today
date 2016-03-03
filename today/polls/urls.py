15a0d0e833143ccbc0550836bc4a0a6e60da3f77
Huang Bo <huangbop@gmail.com>
2016-03-03 22:54:57 +0800

    {% url %}

--- today/polls/urls.py
+++ today/polls/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote),
 ]
