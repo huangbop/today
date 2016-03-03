bf02aa44a721ab8c58bbaedcc78060e6bbc1512d
Huang Bo <huangbop@gmail.com>
2016-03-04 00:17:39 +0800

    Write a simple form

--- today/polls/urls.py
+++ today/polls/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
