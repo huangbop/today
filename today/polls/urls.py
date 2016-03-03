6f831beb7133d6f35828e93ff085d0dd88c2c2e5
Huang Bo <huangbop@gmail.com>
2016-03-03 18:35:30 +0800

    Raise a 404 error

--- today/polls/urls.py
+++ today/polls/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    url(
-        r'^$',
-        views.index,
-    ),
+    url(r'^$', views.index),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote),
 ]
