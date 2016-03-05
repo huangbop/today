85b50c2add20d25754a500601c696f39283b3681
Huang Bo <huangbop@gmail.com>
2016-03-06 01:57:17 +0800

    Testing our new view

--- today/polls/views.py
+++ today/polls/views.py
@@ -4,15 +4,19 @@ from django.template import loader
 from django.http import Http404
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()
+                                       ).order_by('-pub_date')[:5]
+
 
 class DetailView(generic.DetailView):
     model = Question
