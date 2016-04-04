831112d5a039652c6aca90a51d867bd3cec27377
Huang Bo <huangbop@gmail.com>
2016-04-05 01:19:39 +0800

    Show the avatar

--- today/delics/models.py
+++ today/delics/models.py
@@ -8,6 +8,7 @@ class Delic(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     pub_date = models.DateTimeField(auto_now=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=10)
 
     def __str__(self):
         return self.title
