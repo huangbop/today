e3d4b93099a6d0b5544c3a283748eb025ff7ff1e
Huang Bo <huangbop@gmail.com>
2016-04-04 15:31:59 +0800

    Show delics in the content

--- today/delics/admin.py
+++ today/delics/admin.py
@@ -5,6 +5,7 @@ from .models import Delic
 
 class DelicAdmin(admin.ModelAdmin):
     model = Delic
+    list_display = ('title', 'content', 'user', 'pub_date')
 
 
 admin.site.register(Delic, DelicAdmin)
--- today/delics/models.py
+++ today/delics/models.py
@@ -8,3 +8,6 @@ class Delic(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     pub_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
