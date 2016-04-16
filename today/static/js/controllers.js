fa18f8cb61368f1d1be4ef3b16c82406f35945cc
Huang Bo <huangbop@gmail.com>
2016-04-17 02:27:11 +0800

    Routing

--- today/static/js/controllers.js
+++ today/static/js/controllers.js
@@ -1,35 +1,38 @@
+angular.module('contactsMgr', [])
+  .controller('myCtrl',
+              function ($scope, jsonFilter) {
+                $scope.name = 'World';
+                $scope.title = 'Angular tutorial';
+                $scope.author = 'Huang Bo';
 
+                $scope.clickMe = function() {
+                  $scope.isHidden = !$scope.isHidden;
+                };
 
-function myCtrl($scope) {
-  $scope.name = 'World';
-  $scope.title = 'Angular tutorial';
-  $scope.author = 'Huang Bo';
+                $scope.contacts = [{
+                  name: 'Kally',
+                  'phone': '215466464',
+                  'email': 'zzz@zzz.com',
+                }, {
+                  'name': 'The BBB',
+                  'phone': '415657',
+                  'email': 'bbb@bbb.com',
+                }];
 
-  $scope.clickMe = function() {
-    $scope.isHidden = !$scope.isHidden;
-  };
+                $scope.setStyle = function() {
+                  if ($scope.styler) {
+                    return {
+                      'background': 'red',
+                    };
+                  } else
+                    return;
+                };
 
-  $scope.contacts = [
-    {
-      name: 'Kally',
-      'phone': '215466464',
-      'email': 'zzz@zzz.com',
-    },
-    {
-      'name': 'The BBB',
-      'phone': '415657',
-      'email': 'bbb@bbb.com',
-    }
-  ];
-
-  $scope.setStyle = function() {
-    if ($scope.styler) {
-      return {
-        'background': 'red',
-      };
-    } else
-      return;
-  };
-
-}
-
+                console.log(jsonFilter($scope.contacts));
+              }
+             )
+  .filter('truncate', function () {
+    return function (input, limit) {
+      return (input.length > limit) ? input.substr(0, limit)+'...' : input;
+    };
+  });
